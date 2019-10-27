7fbcd552b4caff0a291fae415efa0e5601a4ca93
blackdahila <ola.zxcvbnm@gmail.com>
2019-10-27 18:10:41 +0100

    Remove react-spinners, add own loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,12 @@
 /** @jsx jsx */
-import { ClipLoader } from "react-spinners";
-import { css, jsx } from "@emotion/core";
+import { css, jsx, keyframes } from "@emotion/core";
 import { useTheme } from "../theme";
+
+const spin = keyframes`
+  100% {
+    transform: rotate(360deg);
+  }
+`;
 
 export const Loader = () => {
   const { colors } = useTheme();
@@ -15,7 +20,23 @@ export const Loader = () => {
         padding-top: 20px;
       `}
     >
-      <ClipLoader loading size={80} color={colors.text05} />
+      <div
+        css={css`
+          animation: ${spin} 4s infinite linear;
+        `}
+      >
+        <div
+          css={css`
+            width: 60px;
+            height: 60px;
+            border: 3px solid ${colors.text09};
+            border-top-color: ${colors.text05};
+            border-radius: 50%;
+
+            animation: ${spin} 1.25s infinite ease-in-out;
+          `}
+        />
+      </div>
     </section>
   );
 };
